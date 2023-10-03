bb246d7c273b7a8dafd8bd08444d5bbcfb2b2bf4
CieL2731 <kuroneko7205@gmail.com>
2023-10-03 16:06:09 +0900

    CRUD

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -32,7 +32,12 @@ class PlayersController extends Controller
      */
     public function show($id)
     {
-        //
+        return new Response(
+            Player::query()
+                ->where('id', $id) // 指定したIDに一致するプレイヤーを検索
+                ->select(['id', 'name', 'hp', 'mp', 'money']) // 指定したカラムを選択
+                ->first() // 指定したIDを検索し、一番初めに合致した結果を取得
+        );
     }
 
     /**
@@ -43,7 +48,15 @@ class PlayersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Player::insertGetid(
+            [
+            'id'=>$request->id,
+            'name'=>$request->name,
+            'hp'=>$request->hp,
+            'mp'=>$request->mp,
+            'money'=>$request->money,
+        ]
+        );
     }
 
     /**
@@ -55,7 +68,15 @@ class PlayersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        Player::Where('id',$id)
+        ->update(
+            [
+            'name'=>$request->name,
+            'hp'=>$request->hp,
+            'mp'=>$request->mp,
+            'money'=>$request->money,
+        ]
+        );
     }
 
     /**
@@ -66,7 +87,8 @@ class PlayersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Player::where('id', $id) // 指定したIDに一致するプレイヤーを検索
+        ->delete();
     }
 
     /**
