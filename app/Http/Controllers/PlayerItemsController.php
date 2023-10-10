ea87361a5c101581e89e3845df1951b3d18a92a4
Matsunaga Ikuru <chachamonhan@gmail.com>
2023-10-10 10:45:31 +0900

    関係のないファイルの削除

--- app/Http/Controllers/PlayerItemsController.php
+++ app/Http/Controllers/PlayerItemsController.php
@@ -12,39 +12,6 @@ use Illuminate\Support\Facades\DB; // 追加
 
 class PlayerItemsController extends Controller
 {
-    /*
-    public function addItem(Request $request, $id)
-    {
-        // プレイヤーIDに対応するレコードを取得
-        $playerItem = PlayerItems::where('player_id', $id)
-            ->where('item_id', $request->itemId)
-            ->first();
-
-        // レコードが存在するかどうかで処理を分岐
-        if ($playerItem) 
-        {
-            // 既存のアイテムが存在する場合、countを加算
-            $playerItem->count += $request->count;
-        } 
-        
-        else 
-        {
-            // アイテムが存在しない場合、新しいレコードを挿入
-            $playerItem = new PlayerItems([
-                'player_id' => $id,
-                'item_id' => $request->itemId,
-                'count' => $request->count,
-            ]);
-        }
-
-        // データベースに保存
-        $playerItem->save();
-
-        // レスポンスを返す
-        //return response()->json([]);
-        return response('更新した。');
-    }
-    */
     public function addItem(Request $request, $id)
 {
     // プレイヤーIDとアイテムIDでレコードをデータベースから検索
