1679965ef4a3b2119510c6196c73d1239c166be7
Matsunaga Ikuru <chachamonhan@gmail.com>
2023-10-12 18:21:51 +0900

    追加修正

--- database/migrations/2023_10_11_091851_add_percent_to_items_table.php
+++ database/migrations/2023_10_11_091851_add_percent_to_items_table.php
@@ -14,7 +14,7 @@ class AddPercentToItemsTable extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->unsignedBigInteger('percent')->comment("抽選確率");
+            $table->unsignedInteger('percent')->default(50)->comment("抽選確率");
         });
     }
 
@@ -25,7 +25,8 @@ class AddPercentToItemsTable extends Migration
      */
     public function down()
     {
-            Schema::dropIfExists('items');
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropColumn('percent');
         });
     }
 }
